e5bf81199a3596c4faedbb709ddb71e5792f4264
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-07-10 17:50:12 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { createUserController,createManyUserController, getUserController, getSingleUserController, updateUserController, deleteUserController } = require("../controllers/userController")
+const { createUserController,createManyUserController, getUserController, getSingleUserController, updateUserController,searchUserController, deleteUserController } = require("../controllers/userController")
 const router = express.Router()
 
  //create user
@@ -14,6 +14,9 @@ router.get("/users/:id", getSingleUserController)
 
 //update user
 router.put("/users/:id", updateUserController)
+
+ // search user with name
+router.get("/users/search/:key", searchUserController)
 
 //delete user
 router.delete("/users/:id", deleteUserController)
